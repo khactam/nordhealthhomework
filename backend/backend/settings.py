fc0af7a3e50b61d753967813ec2dac61c11e3cfd
khactam <khactam95@gmail.com>
2022-03-08 20:23:47 +0200

    change endpoint to heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -132,6 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 import dj_database_url
 prod_db=dj_database_url.config(conn_max_age=500)
